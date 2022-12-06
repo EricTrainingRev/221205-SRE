c8d169c05b6ab6826737d2e255d5561a32ff1968
EricTrainingRev <eric.suminski@revature.com>
2022-12-06 15:29:41 -0500

    added more control flow work and a bit of array info

---
+++ Java_Notes/ControlFlowTwo.java
@@ -0,0 +1,63 @@
+public class ControlFlowTwo {
+    public static void main(String[] args) {
+        /*
+         * often you will find yourself needing to execute one of many potential pieces of code depending on 
+         * the data your code is working with. Going back to the idea of getting a visa for your birthday, if 
+         * you don't have enough money on the card to make a purchase you might find it helpful to get back the
+         * actual value on the card
+         */
+
+         int visa = 50;
+         int cost = 50;
+
+         if(visa >= cost){
+            System.out.println("congrats on your purchase");
+         } else {
+            System.out.println("You do not have enough money to make this purchase");
+         }
+
+         /*
+          * the above example is useful for an either/or situation, but we can go further and add even more logical
+          checks to our code
+          */
+
+        // only one of the print messages below will go to the console, depending on which (if any) of the if statements
+        // has its condition satisfied first
+        if(visa > cost){
+            System.out.println("congrats on your purchase");
+        } else if(visa == cost){
+            System.out.println("Purchase made, you have no money left on the gift card");
+        } else {
+            System.out.println("You do not have enough money to make this purchase");
+        }
+
+        // one place you might make use of this is with user authentication
+        String username = "username";
+        String password = "passw0rd";
+
+        // nested if statements can be a little rough to read: I recommend combining them into one if statement
+        // username.equals("username") is the String equivalent of username == "username"
+
+        // if you want to combine logical operations you can either use the logical and && or use the logical
+        // orr || operators
+        if(username.equals("username")){
+            if(password.equals("passw0rd")){
+                System.out.println("logged in successfully");
+            } else {
+                System.out.println("login failed: please try again");
+            }
+        } else {
+            System.out.println("login failed: please try again");
+        }
+
+        if (username.equals("username") && password.equals("passw0rd")){
+            System.out.println("logged in successfully");
+        } else {
+            System.out.println("login failed: please try again");
+        }
+
+
+
+
+    }
+}
---
+++ Java_Notes/Loops.java
@@ -0,0 +1,43 @@
+public class Loops {
+    /*
+     * Loops are another way of controlling the flow of your application. These are particularly useful whe you
+     * need an operation to happen multiple times. The easiest way to make this happen is with a for loop
+     */
+
+     /*
+      * A for loop has three basic parts: a controlling value, a condition under which the loop continues, and a 
+      change to the controlling value
+      */
+
+      public static void main(String[] args) {
+        for(int x = 0; x <= 10; x++){ // ++ is the increment operator, will increase x by one each iteration
+            System.out.println(x);
+        }
+
+        // if you set your loop up incorrectly, you will make an infinite loop and your code will be stuck
+        // you can either use your ide to stop the code or press ctr + c with the terminal selected to kill the process
+        // for(int x = 0; x <= 10; x--){ // -- is the decrement operator, it will reduce the value of x
+        //     System.out.println(x);
+        // }
+
+        // we can combine our loops and if statements to have fine-tune control over our application
+        for(int x = 1; x <= 10; x++){
+            if(x % 2 == 0){ // % is the modulus operator: it returns the remainder of a division operation
+                System.out.println(x);
+            }
+        }
+
+        // if I wanted to store int numbers and loop through them I could do so with an array
+        int[] numbers = {1,3,5,9,15,34,73,8,7893274};
+        // to access the contents of an array you use index positions: indexing starts at 0 and works its way up
+        for(int x = 0; x < numbers.length; x++ ){
+            if(numbers[x] % 2 == 0){
+                System.out.println("x is even this iteration");
+            } else {
+                System.out.println("x is odd this iteration");
+            }
+        }
+
+
+      }
+}
