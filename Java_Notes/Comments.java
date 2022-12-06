eca66207451506ec23fe6f54193a61b20f22e9a4
EricTrainingRev <eric.suminski@revature.com>
2022-12-06 12:22:09 -0500

    added comments and primitive notes

---
+++ Java_Notes/Comments.java
@@ -0,0 +1,11 @@
+public class Comments { // I can declare my class but also write a comment on the same line
+
+    // use two forward slashes to denote everything after them is a comment
+    
+    /*
+     to make a multi-line comment
+     you just place an opening forward slash and star, write out
+     your multi-line comment and then
+     close it with a star forward slash
+     */
+}
---
+++ Java_Notes/Primitives.java
@@ -0,0 +1,23 @@
+public class Primitives {
+    /*
+     * Java represents the most basic of data in structures called primitives: these data types are the simpliest
+     * ones you can work with in Java, and we will be making extensive use of them while we look at the other
+     * aspects of Java
+     */
+
+     // these primitive types represent whole numbers
+     byte b = 0; // 8 bits in size
+     short s = 0; // 16 bits in size
+     int i = 0; // 32 bits in size max size 2^32-1 max size
+     long l = 0; // 64 bits in size
+
+     // these primitive types represent numbers with decimals
+     float f = 0f; // 32 bits in size
+     double d = 0; // 64 bits
+
+     // this primitive represents the unicode character
+     char c = 'c'; // 16 bits
+
+     // this primitive represents logical true or false
+     boolean bb = true; // 1 bit
+}
