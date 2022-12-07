ce7c466e8b052bbc12fd1ec3ef24a4a373613250
EricTrainingRev <eric.suminski@revature.com>
2022-12-07 12:57:47 -0500

    added abstract class and interface examples

---
+++ Java_Notes/Calculator.java
@@ -0,0 +1,42 @@
+public class Calculator implements Maths {
+
+    public int[] convertFloat(float numOne, float numTwo){
+        int[] nums = new int[2];
+        nums[0] = Math.round(numOne);
+        nums[1] = Math.round(numTwo);
+        return nums;
+    }
+
+    @Override
+    public void add(float numOne, float numTwo) {
+        int[] integers = this.convertFloat(numOne, numTwo);
+        System.out.println(integers[0]+integers[1]); // we print the value of our rounded numbers to the console
+        
+    }
+
+    @Override
+    public void subtract(float numOne, float numTwo) {
+        // to turn these floats into ints we could do type casting: this will truncate any decimal values
+        int intOne = (int)numOne;
+        int intTwo = (int)numTwo;
+        System.out.println(intOne-intTwo); // we print the value of our rounded numbers to the console
+        
+    }
+
+    @Override
+    public void multiply(float numOne, float numTwo) {
+        int intOne = Math.round(numOne); // this will convert our float to an int by rounding it
+        int intTwo = Math.round(numTwo);
+        System.out.println(intOne*intTwo); // we print the value of our rounded numbers to the console
+        
+    }
+
+    @Override
+    public void divide(float numOne, float numTwo) {
+        int intOne = Math.round(numOne); // this will convert our float to an int by rounding it
+        int intTwo = Math.round(numTwo);
+        System.out.println(intOne/intTwo); // we print the value of our rounded numbers to the console
+        
+    }
+    
+}
---
+++ Java_Notes/CalculatorPlayground.java
@@ -0,0 +1,10 @@
+public class CalculatorPlayground {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        TI89 betterCalculator = new TI89();
+        calculator.divide(10.1f, 3.2f);
+        betterCalculator.divide(10.1f, 3.2f);
+    }
+    
+}
