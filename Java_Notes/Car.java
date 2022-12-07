2faae5ee24c71ef898c7068dcce549ed80ad947a
EricTrainingRev <eric.suminski@revature.com>
2022-12-07 10:57:22 -0500

    added Car class and CarTrack

---
+++ Java_Notes/Car.java
@@ -0,0 +1,67 @@
+public class Car {
+    // for this example we can pretend we are writing an app for a car videogame
+
+    // All cars are going to share some properties in our code
+    public String owner;
+    public int gas;
+    public static int count; // anytime you see the static keyword in a class know that the field/method is SHARED across all instances of the class
+
+    // All classes have a special method (methods are repeatable code) that handles creating an object
+    // of the class. This is called instantiation.
+
+    // what we are doing here is creating our own constructor that handles setting the value of the owner
+    // field when any car object is created
+    public Car(String startingOwner, int startingGas){
+        // "this" is a reference to the object being created
+        // we are setting whatever the value of startingOwner is to the object's owner field
+        this.owner = startingOwner;
+        this.gas = startingGas;
+        count++; // notice I don't need the "this" keyword, since the field is shared across all objects of the Car class
+    }
+
+    // you can create more than one constructor, the compiler will know how to handle them in your code depending
+    // on the arguments you provide.
+    public Car(){
+        count++; // notice I don't need the "this" keyword, since the field is shared across all objects of the Car class
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Car.count); // this should say 0 because no cars have been made at this point
+
+        Car georgeClooneyCar = new Car(); // here we call the no args constructor to make a Car object with no fields set
+        System.out.println(Car.count); // this should now say 1 because there is one car object created in our code
+
+        georgeClooneyCar.owner = "George Clooney"; // here we set the value of the myCar Car object field owner to "george clooney"
+
+        System.out.println(georgeClooneyCar.owner); // here we confirm that we set the value of the owner field in the myCar object
+
+        Car mattDamonCar = new Car("Matt Damon", 100); // here we call the constructor that initializes the owner field of the object
+        System.out.println(Car.count); // this should now say 2 because there are two car objects created in our code
+
+        System.out.println(mattDamonCar.gas); // we can see the results without having to manually set the value in a second line of code
+        System.out.println(georgeClooneyCar.gas);
+        // to call the drive method I MUST have a Car object to do it: I can't call the method by itself
+        mattDamonCar.drive(); // make sure not to forget the () when you call a method
+
+        System.out.println(mattDamonCar.gas); // because we made the drive method reduce the gas of the car the gas field is now lower
+        System.out.println(georgeClooneyCar.gas);
+
+        Car.checkCount();
+    }
+
+    /*
+     * public: access modifier, in this case, we are saying this method can be called by any object anywhere in our code
+     * void: return type, in this case, we are saying this method does not return any data
+     * drive: name of our method, method names should give a decent idea of what is being accomplished by your code
+     */
+    public void drive(){
+        System.out.println("vroom vroom!");
+        this.gas-=10;
+    }
+
+    // by adding the static keyword to the method we indicate the method belongs to the class proper, and so is called by the class, not individual objects
+    public static void checkCount(){
+        System.out.println("there are " + count + " cars created" );
+    }
+
+}
--- Java_Notes/Java-Intro.md
+++ Java_Notes/Java-Intro.md
@@ -17,3 +17,6 @@ The Java Development Kit (JDK) contains the libraries and tools we need to be ab
 A Java Runtime Environment (JRE) is provided with the JDK when you download it. A JRE is what is needed to actually be able to run your Java application. The JRE contains the runtime libraries necessary for your code to execute. Contained within the JRE is the Java Virtual Machine(JVM), which is the tool that actually handles ensuring your code will run on your machine
 
 The JVM is the tool that takes the byte code (.class files) and turns it into data that is understanable by your particular operating system. Keep in mind, all JVMs are found in their respective Java Runtime Environment
+
+## Classes & Objects
+Java is an Object Oriented Programming language: this means that everything is tied to classes and objects. Classes in Java are like blueprints: they contain the information needed for your code to work. Objects in Java are the implementation of the blueprints. It is important to remember that objects do not share the value of their fields with one another: the values are unique to the object when these fields are part of the instance/object scope.
