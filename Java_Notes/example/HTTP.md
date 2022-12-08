9121bb78626b718612c4b5cee7655fa84d929a8b
EricTrainingRev <eric.suminski@revature.com>
2022-12-08 15:02:05 -0500

    added more Javalin examples

---
+++ Java_Notes/example/HTTP.md
@@ -0,0 +1,26 @@
+# What is HTTP?
+Hyper Text Transfer Protocol (http) is the most popular way of transfering information across the web in modern times. It is a request/response system that guarantees a response for every request made. There are a few standard components of an HTTP request you should be familiar with:
+- uri
+    - this is the part of the request that tells the web server what information you want
+- body
+    - many http requests are going to contain a body: this is the information that is needed to complete whatever request you are making
+- verb
+    - the "verb" of an http request provides context for what you as the requester are trying to accomplish with your http request
+        - GET: the requester wants to "get" some kind of information
+        - POST: typically used when the request wants to create some kind of entry or provide new information to the web application
+        - PUT: typically used when the requester wants to update ALL of some resource handled by the web application
+        - PATCH: typically when the requester wants to update SOME of a resource handled by the web application
+        - DELETE: typically used when the requester wants to delete some resource handled by the web application
+
+You also should be familiar with the following components of an http response:
+- body
+    - this contains whatever information you requested (or it could be an error message)
+- status code [more info on status codes](https://developer.mozilla.org/en-US/docs/Web/HTTP/Status)
+    - this is a number that gives an immediate indication of how your request was handled
+    - there are multiple http status levels you can get
+        - 100 is informational
+        - 200 is success
+        - 300 is redirect
+        - 400 is user error (clientside error)
+        - 500 is server error (the developer messed up)
+            - 500 is BAD: you do not want 500 status codes being returned to your users
--- Java_Notes/example/src/main/java/com/example/App.java
+++ Java_Notes/example/src/main/java/com/example/App.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.javalin.Javalin;
 
 /*
@@ -12,10 +15,47 @@ import io.javalin.Javalin;
 
 public class App 
 {
+    public static List<String> names = new ArrayList<>();
     public static void main( String[] args ){
+
+        // adding some names to the names list so we can interact with them via http
+        names.add("Billy");
+        names.add("Sally");
+        names.add("Bruce");
+
+
         Javalin app = Javalin.create();
 
+        /*
+         * the code below tells Javalin that our web server that is created needs to be able to handle a
+         * GET http request that ends with /hello. Upon our application recieving this request the application 
+         * should return the text "Hello world!" to the requester
+         */
         app.get("/hello", ctx -> ctx.result("Hello world!"));
+
+        app.post("/add", ctx -> {
+            // we get the name from the http request body and save it to a variable
+            String newName = ctx.body();
+            // we add the new name into the list of names
+            names.add(newName);
+            // we tell Javalin we want to return a String message to the requester
+            ctx.result("Congrats! You added a new name to the names database");
+            // we tell Javalin we want the status code 201 returned in the response
+            ctx.status(201);
+        });
+
+        app.get("/person/{num}", ctx -> {
+            // we use the pathParam method to get the value of the num path parameter
+            String numAsString = ctx.pathParam("num");
+            // since the value is a number, we convert the string into an int
+            int num = Integer.parseInt(numAsString);
+            // we use our newly converted int to access the index position of the name we want
+            String person = names.get(num);
+            // we tell javalin to return the name of the person we got
+            ctx.result(person);
+            // we tell Javalin to set the status code to 200
+            ctx.status(200);
+        });
 
         app.start();
     }
